cd35b64b1d28589758019b3b02db2ebc0475cc5a
Xing <116251968+Xingeriaa@users.noreply.github.com>
2023-12-17 15:41:53 +0700

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -5,22 +5,27 @@ local Character = LocalPlayer.Character;
 
 local PlantRange = 20;
 
-_G.InToggle = false;
-RunService.Stepped:Connect(function()
-    if _G.InToggle == false then
-        _G.InToggle = true;
+-- _G.InToggle = false;
+-- RunService.Stepped:Connect(function()
+--     if _G.InToggle == false then
+--         _G.InToggle = true;
         local HRP = Character:FindFirstChild("HumanoidRootPart");
         if HRP == true then
             for i,v in pairs(workspace.Deployables:GetChildren()) do
                 if v ~= nil and v.Name == "Plant Box" and v.Part ~= nil and math.round((HRP.Position - v.Part.Position).magnitude) <= PlantRange then 
                     if v:FindFirstChild("Bloodfruit") ~= nil then 
-                        game:GetService("ReplicatedStorage").Events.InteractStructure:FireServer(v,"Bloodfruit")
-
+                        local args = {
+                            [1] = v,
+                            [2] = "Bloodfruit"
+                        }
+                        
+                        game:GetService("ReplicatedStorage").Events.InteractStructure:FireServer(unpack(args))
+                        
                     end
                 end
             end
         end
-        _G.InToggle = false;
-    end
-end)
+--         _G.InToggle = false;
+--     end
+-- end)
 
